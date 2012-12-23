bacbba3ad7f012d26b5d608356bd75702927d454
KyleCartmell <KyleCartmel@gmail.com>
2012-12-23 16:21:45 -0700

    maven plugin can now execute tests on an android device

--- soong-maven-plugin/src/main/java/net/wakka/soong/IntegrationTest.java
+++ soong-maven-plugin/src/main/java/net/wakka/soong/IntegrationTest.java
@@ -1,5 +1,7 @@
 package net.wakka.soong;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -7,14 +9,29 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
+import java.io.IOException;
 
 @Mojo( name = "integration-test", defaultPhase = LifecyclePhase.INTEGRATION_TEST)
 public class IntegrationTest extends AbstractMojo {
 
+  private static final String START_SERVICE_COMMAND =
+      "adb shell am startservice ";
+
+  // net.wakka.soong.examples.twitter/.FoobarService
+  @Parameter( property = "integrationTestRunnerServiceIntent", required = true )
+  private String intent;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    System.out.println("### ### ### ### HELLO, WORLD!!");
+    int exitValue = 0;
+    try {
+      exitValue = new DefaultExecutor().execute(CommandLine.parse(START_SERVICE_COMMAND + intent));
+      if(exitValue != 0) {
+        throw new MojoExecutionException("I think I failed to execute the integration test runner service. (exit code was " + exitValue + ")");
+      }
+    } catch (IOException e) {
+      throw new MojoExecutionException("Something bad happened.",e);
+    }
   }
 
 }
